d07c9cb7166d9358940a24a3e3f92370ad51f295
artday <admin@artday.com.ua>
2018-05-29 03:22:05 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
